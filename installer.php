2b391cc5fca7b40893e08ef9c467e55eaaa6cd0a
Stefan Hüsges <tronsha@gmx.de>
2017-01-12 08:40:13 +0100

    normalize.css v5.0.0

--- installer.php
+++ installer.php
@@ -36,33 +36,33 @@ $config = array(
     'src' => array(
         /** @link https://wordpress.org/download/release-archive/ */
         'latest' => 'https://wordpress.org/latest.zip',
-        '4.7' => 'https://wordpress.org/wordpress-4.7.zip',
-        '4.6' => 'https://wordpress.org/wordpress-4.6.1.zip',
-        '4.5' => 'https://wordpress.org/wordpress-4.5.4.zip',
-        '4.4' => 'https://wordpress.org/wordpress-4.4.5.zip',
-        '4.3' => 'https://wordpress.org/wordpress-4.3.6.zip',
-        '4.2' => 'https://wordpress.org/wordpress-4.2.10.zip',
-        '4.1' => 'https://wordpress.org/wordpress-4.1.13.zip',
-        '4.0' => 'https://wordpress.org/wordpress-4.0.13.zip',
+        '4.7' => 'https://wordpress.org/wordpress-4.7.1.zip',
+        '4.6' => 'https://wordpress.org/wordpress-4.6.2.zip',
+        '4.5' => 'https://wordpress.org/wordpress-4.5.5.zip',
+        '4.4' => 'https://wordpress.org/wordpress-4.4.6.zip',
+        '4.3' => 'https://wordpress.org/wordpress-4.3.7.zip',
+        '4.2' => 'https://wordpress.org/wordpress-4.2.11.zip',
+        '4.1' => 'https://wordpress.org/wordpress-4.1.14.zip',
+        '4.0' => 'https://wordpress.org/wordpress-4.0.14.zip',
         /** @link https://de.wordpress.org/releases/ */
         'latest.de' => 'https://de.wordpress.org/latest-de_DE.zip',
-        '4.7.de' => 'https://de.wordpress.org/wordpress-4.7-de_DE.zip',
-        '4.6.de' => 'https://de.wordpress.org/wordpress-4.6.1-de_DE.zip',
-        '4.5.de' => 'https://de.wordpress.org/wordpress-4.5.4-de_DE.zip',
-        '4.4.de' => 'https://de.wordpress.org/wordpress-4.4.5-de_DE.zip',
-        '4.3.de' => 'https://de.wordpress.org/wordpress-4.3.6-de_DE.zip',
-        '4.2.de' => 'https://de.wordpress.org/wordpress-4.2.10-de_DE.zip',
-        '4.1.de' => 'https://de.wordpress.org/wordpress-4.1.13-de_DE.zip',
+        '4.7.de' => 'https://de.wordpress.org/wordpress-4.7.1-de_DE.zip',
+        '4.6.de' => 'https://de.wordpress.org/wordpress-4.6.2-de_DE.zip',
+        '4.5.de' => 'https://de.wordpress.org/wordpress-4.5.5-de_DE.zip',
+        '4.4.de' => 'https://de.wordpress.org/wordpress-4.4.6-de_DE.zip',
+        '4.3.de' => 'https://de.wordpress.org/wordpress-4.3.7-de_DE.zip',
+        '4.2.de' => 'https://de.wordpress.org/wordpress-4.2.11-de_DE.zip',
+        '4.1.de' => 'https://de.wordpress.org/wordpress-4.1.14-de_DE.zip',
         '4.0.de' => 'https://de.wordpress.org/wordpress-4.0.12-de_DE.zip',
         /** @link https://nl.wordpress.org/releases/ */
         'latest.nl' => 'https://nl.wordpress.org/latest-nl_NL.zip',
-        '4.7.nl' => 'https://nl.wordpress.org/wordpress-4.7-nl_NL.zip',
+        '4.7.nl' => 'https://nl.wordpress.org/wordpress-4.7.1-nl_NL.zip',
         '4.6.nl' => 'https://nl.wordpress.org/wordpress-4.6.1-nl_NL.zip',
         '4.5.nl' => 'https://nl.wordpress.org/wordpress-4.5.3-nl_NL.zip',
         '4.4.nl' => 'https://nl.wordpress.org/wordpress-4.4.2-nl_NL.zip',
-        '4.3.nl' => 'https://nl.wordpress.org/wordpress-4.3.6-nl_NL.zip',
-        '4.2.nl' => 'https://nl.wordpress.org/wordpress-4.2.10-nl_NL.zip',
-        '4.1.nl' => 'https://nl.wordpress.org/wordpress-4.1.13-nl_NL.zip',
+        '4.3.nl' => 'https://nl.wordpress.org/wordpress-4.3.7-nl_NL.zip',
+        '4.2.nl' => 'https://nl.wordpress.org/wordpress-4.2.11-nl_NL.zip',
+        '4.1.nl' => 'https://nl.wordpress.org/wordpress-4.1.14-nl_NL.zip',
         '4.0.nl' => 'https://nl.wordpress.org/wordpress-4.0.1-nl_NL.zip',
         /** @link https://fr.wordpress.org/releases/ */
         'latest.fr' => 'https://fr.wordpress.org/latest-fr_FR.zip',
@@ -958,10 +958,11 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
         box-sizing: border-box;
     }
 
-    /*! normalize.css v3.0.3 | MIT License | github.com/necolas/normalize.css */
+    /*! normalize.css v5.0.0 | MIT License | github.com/necolas/normalize.css */
 
     html {
         font-family: sans-serif;
+        line-height: 1.15;
         -ms-text-size-adjust: 100%;
         -webkit-text-size-adjust: 100%;
     }
@@ -970,42 +971,8 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
         margin: 0;
     }
 
-    article, aside, details, figcaption, figure, footer, header, hgroup, main, menu, nav, section, summary {
+    article, aside, footer, header, nav, section {
         display: block;
-    }
-
-    audio, canvas, progress, video {
-        display: inline-block;
-        vertical-align: baseline;
-    }
-
-    audio:not([controls]) {
-        display: none;
-        height: 0;
-    }
-
-    [hidden], template {
-        display: none;
-    }
-
-    a {
-        background-color: transparent;
-    }
-
-    a:active, a:hover {
-        outline: 0;
-    }
-
-    abbr[title] {
-        border-bottom: 1px dotted;
-    }
-
-    b, strong {
-        font-weight: bold;
-    }
-
-    dfn {
-        font-style: italic;
     }
 
     h1 {
@@ -1013,8 +980,59 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
         margin: 0.67em 0;
     }
 
+    figcaption, figure, main {
+        display: block;
+    }
+
+    figure {
+        margin: 1em 40px;
+    }
+
+    hr {
+        box-sizing: content-box;
+        height: 0;
+        overflow: visible;
+    }
+
+    pre {
+        font-family: monospace, monospace;
+        font-size: 1em;
+    }
+
+    a {
+        background-color: transparent;
+        -webkit-text-decoration-skip: objects;
+    }
+
+    a:active, a:hover {
+        outline-width: 0;
+    }
+
+    abbr[title] {
+        border-bottom: none;
+        text-decoration: underline;
+        text-decoration: underline dotted;
+    }
+
+    b, strong {
+        font-weight: inherit;
+    }
+
+    b, strong {
+        font-weight: bolder;
+    }
+
+    code, kbd, samp {
+        font-family: monospace, monospace;
+        font-size: 1em;
+    }
+
+    dfn {
+        font-style: italic;
+    }
+
     mark {
-        background: #ff0;
+        background-color: #ff0;
         color: #000;
     }
 
@@ -1029,47 +1047,39 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
         vertical-align: baseline;
     }
 
-    sup {
-        top: -0.5em;
-    }
-
     sub {
         bottom: -0.25em;
     }
 
+    sup {
+        top: -0.5em;
+    }
+
+    audio, video {
+        display: inline-block;
+    }
+
+    audio:not([controls]) {
+        display: none;
+        height: 0;
+    }
+
     img {
-        border: 0;
+        border-style: none;
     }
 
     svg:not(:root) {
         overflow: hidden;
     }
 
-    figure {
-        margin: 1em 40px;
-    }
-
-    hr {
-        box-sizing: content-box;
-        height: 0;
-    }
-
-    pre {
-        overflow: auto;
-    }
-
-    code, kbd, pre, samp {
-        font-family: monospace, monospace;
-        font-size: 1em;
-    }
-
     button, input, optgroup, select, textarea {
-        color: inherit;
-        font: inherit;
+        font-family: sans-serif;
+        font-size: 100%;
+        line-height: 1.15;
         margin: 0;
     }
 
-    button {
+    button, input {
         overflow: visible;
     }
 
@@ -1077,40 +1087,17 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
         text-transform: none;
     }
 
-    button, html input[type="button"], input[type="reset"], input[type="submit"] {
+    button, html [type="button"], [type="reset"], [type="submit"] {
         -webkit-appearance: button;
-        cursor: pointer;
     }
 
-    button[disabled], html input[disabled] {
-        cursor: default;
-    }
-
-    button::-moz-focus-inner, input::-moz-focus-inner {
-        border: 0;
+    button::-moz-focus-inner, [type="button"]::-moz-focus-inner, [type="reset"]::-moz-focus-inner, [type="submit"]::-moz-focus-inner {
+        border-style: none;
         padding: 0;
     }
 
-    input {
-        line-height: normal;
-    }
-
-    input[type="checkbox"], input[type="radio"] {
-        box-sizing: border-box;
-        padding: 0;
-    }
-
-    input[type="number"]::-webkit-inner-spin-button, input[type="number"]::-webkit-outer-spin-button {
-        height: auto;
-    }
-
-    input[type="search"] {
-        -webkit-appearance: textfield;
-        box-sizing: content-box;
-    }
-
-    input[type="search"]::-webkit-search-cancel-button, input[type="search"]::-webkit-search-decoration {
-        -webkit-appearance: none;
+    button:-moz-focusring, [type="button"]:-moz-focusring, [type="reset"]:-moz-focusring, [type="submit"]:-moz-focusring {
+        outline: 1px dotted ButtonText;
     }
 
     fieldset {
@@ -1120,25 +1107,64 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
     }
 
     legend {
-        border: 0;
+        box-sizing: border-box;
+        color: inherit;
+        display: table;
+        max-width: 100%;
         padding: 0;
+        white-space: normal;
+    }
+
+    progress {
+        display: inline-block;
+        vertical-align: baseline;
     }
 
     textarea {
         overflow: auto;
     }
 
-    optgroup {
-        font-weight: bold;
-    }
-
-    table {
-        border-collapse: collapse;
-        border-spacing: 0;
-    }
-
-    td, th {
+    [type="checkbox"], [type="radio"] {
+        box-sizing: border-box;
         padding: 0;
+    }
+
+    [type="number"]::-webkit-inner-spin-button, [type="number"]::-webkit-outer-spin-button {
+        height: auto;
+    }
+
+    [type="search"] {
+        -webkit-appearance: textfield;
+        outline-offset: -2px;
+    }
+
+    [type="search"]::-webkit-search-cancel-button, [type="search"]::-webkit-search-decoration {
+        -webkit-appearance: none;
+    }
+
+    ::-webkit-file-upload-button {
+        -webkit-appearance: button;
+        font: inherit;
+    }
+
+    details, menu {
+        display: block;
+    }
+
+    summary {
+        display: list-item;
+    }
+
+    canvas {
+        display: inline-block;
+    }
+
+    template {
+        display: none;
+    }
+
+    [hidden] {
+        display: none;
     }
 
     /* Installer Layout */
@@ -1181,6 +1207,10 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
     td, th {
         font-weight: normal;
         text-align: left;
+    }
+
+    button, input, optgroup, select, textarea {
+        font-family: 'Ubuntu Mono';
     }
 
     .box {
